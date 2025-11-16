e9f86f4bfe40b5c63f20a9ed43ccdd33caf5b67b
DmitroKozachok <149708709+DmitroKozachok@users.noreply.github.com>
2025-11-16 22:34:03 +0200

    Add SMTP

---
+++ SpringBoot/src/main/java/org/example/data/smtp/EmailConfiguration.java
@@ -0,0 +1,14 @@
+package org.example.data.smtp;
+
+public class EmailConfiguration {
+    // Email, з якого відправляємо
+    public static final String FROM = "sendmessage1@ukr.net";
+    // SMTP-сервер
+    public static final String SMTP_SERVER = "smtp.ukr.net";
+    // Порт
+    public static final int PORT = 465;
+    // Ім’я користувача
+    public static final String USERNAME = "sendmessage1@ukr.net";
+    // Пароль користувача
+    public static final String PASSWORD = "yvDvv6Xcm9Ci0tTq";
+}
